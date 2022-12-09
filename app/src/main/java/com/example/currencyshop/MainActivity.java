3a045e78aac17ba41cd29b773b2cdcb939e358a6
HolySquid <89538962+HolySquid@users.noreply.github.com>
2022-12-09 11:48:41 +0600

    Initial commit

---
+++ app/src/main/java/com/example/currencyshop/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.currencyshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import java.text.BreakIterator;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void IncreaseQuantity(View view) {R.id.prise5);
+        BreakIterator IncreaseQuantity;
+        IncreaseQuantity.setText("1");
+    }
+}
