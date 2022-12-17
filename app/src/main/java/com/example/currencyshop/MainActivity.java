6933ea33bbf65b62c6045ac324dc33ca0697e450
HolySquid <89538962+HolySquid@users.noreply.github.com>
2022-12-17 19:12:17 +0600

    Форматирование

--- app/src/main/java/com/example/currencyshop/MainActivity.java
+++ app/src/main/java/com/example/currencyshop/MainActivity.java
@@ -4,19 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import java.text.BreakIterator;
 
 public class MainActivity extends AppCompatActivity {
 
+    int quantity = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void IncreaseQuantity(View view) {R.id.prise5);
-        BreakIterator IncreaseQuantity;
-        IncreaseQuantity.setText("1");
+    public void IncreaseQuantity(View view) {
+        quantity = quantity + 1;
+        TextView QuantityTextView = findViewById(R.id.prise5);
+        QuantityTextView.setText("quantity");
     }
+
 }
